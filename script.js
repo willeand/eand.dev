d63f767859f379feaf9d5157c5e365235237c5e4
willeand <eandwse@gmail.com>
2025-09-22 13:56:02 -0700

    created pages for index, library, projects, and work. Created .active class for nav links, added a script to detect current page, adds .active class to corresponding nav link dynamically

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
     console.log('Welcome to My New Website!');
+    const navLinks = document.querySelectorAll('.NavBox nav a');
+    const currentPage = window.location.pathname.split("/").pop();
+
+    navLinks.forEach(link => {
+        const linkPage = link.getAttribute('href').split("/").pop();
+
+        if (linkPage === currentPage) {
+            link.classList.add('active');    // add the class
+        } else {
+            link.classList.remove('active'); // remove from others
+        }
+    });
 });
